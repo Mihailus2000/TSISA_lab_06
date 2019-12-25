29a1430f3d5c11cb951a5c7fce003556a43e10cf
mihailus2000 <Mihail14112000@mail.ru>
2019-12-25 23:43:46 +0300

    fix1

--- main.cpp
+++ main.cpp
@@ -16,10 +16,10 @@ double DetermineFunction(double x){
     return sin(x) + 0.5;
 }
 
-double Omega(int K, std::vector<double> noisedFunction){
+double Omega(int K, std::vector<double> filteredFunction){
     double  res = 0.;
     for(int k = 1; k < K; k++){
-        res += std::abs(noisedFunction.at(k) - noisedFunction.at(k - 1));
+        res += std::abs(filteredFunction.at(k) - filteredFunction.at(k - 1));
     }
     return res;
 }
@@ -81,13 +81,7 @@ std::vector<double> Filtration(int K, int M, std::vector<double> noisedFunction,
         filteredFunction.push_back(res);
         res = 1.;
     }
-//    if(cnt == 0) {
-//        for (int i = 0; i < filteredFunction.size(); i++) {
-//            filter << '(' << sequence.at(i) << ";" << filteredFunction.at(i) << ')' << std::endl;
-//        }
-//        filter << "-----------------------------\n";
-//        cnt++;
-//    }
+
     return filteredFunction;
 }
 
@@ -122,28 +116,28 @@ double Distance(double omega, double delta){
     return std::abs(omega) + std::abs(delta);
 }
 
-std::tuple<std::vector<double >,double,double, double> FindOptimalForLambda(int n, int lambda, int K, int r, std::vector<double> sequence, std::vector<double> noisedFunction){
+std::tuple<std::vector<double >,std::vector<double>, double, double, double> FindOptimalForLambda(int n, int lambda, int K, int r, std::vector<double> sequence, std::vector<double> noisedFunction){
     double minimum = 1000.;
-    std::tuple<std::vector<double >,double,double, double> optimal; // {vectorAlpha, J, omega, delta}
-    std::vector<double> filteredFunction;
+    std::tuple<std::vector<double >, std::vector<double >, double,double, double> optimal; // {vectorAlpha, J, omega, delta}
+    std::vector<double> bestFilteredFunction;
     for(int i = 0; i < n; i++){
         std::vector<double> alphaVector = GenerateAlpha(r);
-        filteredFunction = Filtration(K, std::floor((r - 1) / 2), noisedFunction, alphaVector, sequence);
-        double omega = Omega(K,filteredFunction);
-        double delta = Delta(K,filteredFunction,noisedFunction);
+        bestFilteredFunction = Filtration(K, std::floor((r - 1) / 2), noisedFunction, alphaVector, sequence);
+        double omega = Omega(K, bestFilteredFunction);
+        double delta = Delta(K, bestFilteredFunction, noisedFunction);
 
         double J = lambda * omega + (1-lambda) * delta;
 
         if(J < minimum){
             minimum = J;
-            optimal = {alphaVector, J, omega, delta};
+            optimal = {bestFilteredFunction, alphaVector, J, omega, delta};
         }
 
         cnt++;
     }
     if(cnt == 0) {
-        for (int i = 0; i < filteredFunction.size(); i++) {
-            filter << '(' << sequence.at(i) << ";" << filteredFunction.at(i) << ')' << std::endl;
+        for (int i = 0; i < bestFilteredFunction.size(); i++) {
+            filter << '(' << sequence.at(i) << ";" << bestFilteredFunction.at(i) << ')' << std::endl;
         }
         filter << "-----------------------------\n";
         cnt++;
@@ -152,24 +146,31 @@ std::tuple<std::vector<double >,double,double, double> FindOptimalForLambda(int 
     return optimal;
 }
 
-double FindOptimalLambda(int L, int K, int r, std::vector<double> sequence){
+// return = {lambda, J, omega, delta}
+std::tuple<double,double,double,double> FindOptimalLambda(int L, int K, int r, std::vector<double> sequence){
     double minimumJ = 100000000.;
     double optimalLambda = -1.;
-
+    std::vector<double> theBestFilteredFunction;
     auto noisedFunction = GenerateNoisedFunction(sequence);
     double dist;
+    double bestJ = 0., bestW = 0., bestD = 0.;
     for(int ind = 0 ; ind <= L; ind++){
         double lambda = static_cast<double>(ind * 1.0) / L;
         std::vector<double> alpha;
+        std::vector<double > bestFilteredFunction;
         double omega, delta, j;
-        std::tie(alpha,j,omega,delta) = FindOptimalForLambda(DetermineAmountOfExperiments(0.95, 0.01), lambda, K, r, sequence, noisedFunction);
+        std::tie(bestFilteredFunction, alpha,j,omega,delta) = FindOptimalForLambda(DetermineAmountOfExperiments(0.95, 0.01), lambda, K, r, sequence, noisedFunction);
         dist = Distance(omega,delta);
         if (dist < minimumJ) {
             minimumJ = dist;
             optimalLambda = lambda;
+            theBestFilteredFunction = bestFilteredFunction;
+            bestJ = j;
+            bestW = omega;
+            bestD = delta;
         }
 
-        std::cout << std::fixed << "| " << std::setprecision(4) << lambda << " | " << j << " |";
+        std::cout << std::fixed << "| " << std::setprecision(4) << lambda << " | " << j << " | " << dist << " |";
         std::cout << " [";
         for(auto alpEl = alpha.begin(); alpEl != alpha.end(); alpEl++){
             std::cout << std::fixed << std::setprecision(4) << *alpEl;
@@ -178,24 +179,45 @@ double FindOptimalLambda(int L, int K, int r, std::vector<double> sequence){
         }
         std::cout << "] | ";
 
-        std::cout << std::fixed << std::setprecision(4) << omega << " | " << delta << " |\n";
+        std::cout << std::fixed << std::setprecision(5) << omega << " | " << delta << " |\n";
     }
-    std::cout << "\n\n";
-    return optimalLambda;
+
+    for (int i = 0; i < theBestFilteredFunction.size(); i++) {
+        filter << '(' << sequence.at(i) << ";" << theBestFilteredFunction.at(i) << ')' << std::endl;
+    }
+    filter << "-----------------------------\n";
+
+    return {optimalLambda, bestJ, bestW, bestD};
 }
 
+void DisplayResult( double theBestOmega, double theBestDelta, double theBestJ, double theBestLambda){
+    std::cout << "+--------+-----------+----------+----------+\n"
+                 "| lambda |     J     |  omega   |   delta  |\n"
+                 "+--------+-----------+----------+----------+\n";
+    std::cout << "|  " << std::fixed << std::setprecision(2) << theBestLambda << "  |  " << std::setprecision(5) <<
+              theBestJ << "  |  " << std::setprecision(4) << theBestOmega  << "  |  " << std::setprecision(4) << theBestDelta << "  |\n";
+    std::cout << "+--------+-----------+----------+----------+\n";
+}
 
 int main() {
-    std::cout << "+--------+--------+--------------------------+---------+--------+\n"
-                 "| lambda |    J   |            alpha         |    w    |    d   |\n"
-                 "+--------+--------+--------------------------+---------+--------+\n";
-    auto optimalLambda = FindOptimalLambda(10,10,3,DiscreteSequence(0.,M_PI, 100));
-    cnt = 0;
-    std::cout << "Optimal lambda for 'r' = 3 : " << std::fixed << std::setprecision(4) << optimalLambda;
+    std::cout << "+--------+--------+--------+--------------------------+---------+---------+\n"
+                 "| lambda |    J   |  dist  |            alpha         |    w    |    d    |\n"
+                 "+--------+--------+--------+--------------------------+---------+---------+\n";
+//    std::vector<double> theBestAlpha;
+    double theBestOmega, theBestDelta, theBestJ, theBestLambda;
+
+    std::tie(theBestLambda,theBestJ,theBestOmega,theBestDelta) = FindOptimalLambda(10,100,3,DiscreteSequence(0.,M_PI, 100));
+    std::cout << "+--------+--------+--------------------------+---------+--------+\n";
+    std::cout << "Optimal result for r = 3:\n\n";
+    DisplayResult(theBestOmega, theBestDelta, theBestJ, theBestLambda);
+
     std::cout << "\n\n";
-    std::cout << "+--------+--------+------------------------------------------+---------+--------+\n"
-                 "| lambda |    J   |                  alpha                   |    w    |    d   |\n"
-                 "+--------+--------+------------------------------------------+---------+--------+\n";
-    auto optimalLambda2 = FindOptimalLambda(10,10,5,DiscreteSequence(0.,M_PI, 100));
-    std::cout << "Optimal lambda for 'r' = 5 : " << std::fixed << std::setprecision(4) << optimalLambda2;
+    std::cout << "+--------+--------+--------+------------------------------------------+---------+---------+\n"
+                 "| lambda |    J   |  dist  |                  alpha                   |    w    |    d    |\n"
+                 "+--------+--------+--------+------------------------------------------+---------+---------+\n";
+    std::tie(theBestLambda,theBestJ,theBestOmega,theBestDelta) = FindOptimalLambda(10,100,5,DiscreteSequence(0.,M_PI, 100));
+    std::cout << "+--------+--------+------------------------------------------+---------+--------+\n";
+    std::cout << "Optimal result for r = 5:\n\n";
+    DisplayResult(theBestOmega, theBestDelta, theBestJ, theBestLambda);
+
 }
